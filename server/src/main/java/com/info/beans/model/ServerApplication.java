2814205afa32f5abad82590a9f9bf8f0ce2f4937
ayappasigatapu143 <ayappa.sigatapu143gmail.com>
2020-04-16 23:10:16 +0530

    recent changes

--- server/src/main/java/com/info/beans/model/ServerApplication.java
+++ server/src/main/java/com/info/beans/model/ServerApplication.java
@@ -12,9 +12,7 @@ public class ServerApplication {
   
  	 public static void main(String[] args) {
 
-         System.out.println(args[0]);
-        System.out.println("sacjasnvjnsajv++++++++++++++++++++++++++++++++++++++++++++"+args[0]);
-
+        
                 
 		SpringApplication.run(ServerApplication.class, args);
 	}
